463d8038077a276dc53b781e94c2dfc8d918adee
Vilík Uzdichcal <bednar.matej2@seznam.cz>
2025-02-09 12:58:04 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,22 +9,22 @@ loadscreen 'html/index.html'
 
 files {
     'html/index.html',
-	'html/assets/images/polarbear.png',
+    'html/assets/images/polarbear.png',
     'html/assets/images/serverlogo.png',
     'html/assets/images/background1.jpg',
     'html/assets/images/background2.jpg',
     'html/assets/images/background3.jpg',
     'html/assets/images/background4.jpg',
     'html/css/style.css',
-	'html/js/script.js',
-	'html/assets/song/song.mp3'
+    'html/js/script.js',
+    'html/assets/song/song.mp3'
 }
 
 loadscreen_manual_shutdown 'yes'
 loadscreen_cursor 'yes'
 
 client_script 'client/client.lua'
-server_script 'server/server.lua'
+server_script 'server/updater.lua'
 shared_script 'shared/config.lua'
 
 -- Do not change anything here!!!
