e2ad270041cda907a62415a7f22dee32efbf6a8e
Vilík Uzdichcal <bednar.matej2@seznam.cz>
2025-02-11 12:13:28 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,15 +9,15 @@ loadscreen 'html/index.html'
 
 files {
     'html/index.html',
-	'html/assets/images/polarbear.png',
+    'html/assets/images/polarbear.png',
     'html/assets/images/serverlogo.png',
     'html/assets/images/background1.jpg',
     'html/assets/images/background2.jpg',
     'html/assets/images/background3.jpg',
     'html/assets/images/background4.jpg',
     'html/css/style.css',
-	'html/js/script.js',
-	'html/assets/song/song.mp3'
+    'html/js/script.js',
+    'html/assets/song/song.mp3'
 }
 
 loadscreen_manual_shutdown 'yes'
